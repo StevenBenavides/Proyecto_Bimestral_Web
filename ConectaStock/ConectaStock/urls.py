840bbd31a16009666b570b4e247864244a6ad77b
ntnsergi <sergi.galarzaend@gmail.com>
2026-07-29 14:02:04 -0500

    ajustes

--- ConectaStock/ConectaStock/urls.py
+++ ConectaStock/ConectaStock/urls.py
@@ -19,9 +19,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('aplicacion.urls')),
+    
+    # Forzar a Django a servir Media Files en producción (Render) para la presentación
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
 
 if settings.DEBUG:
